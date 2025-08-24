84a57788ecbedbcd78c60c5167f7f476372c291d
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-08-24 21:15:36 +0200

    comment old code
    Try to make  new one form code with Ivaylo Papazov

--- index.js
+++ index.js
@@ -1,111 +1,113 @@
-const http = require('http');
-const querystring = require('querystring');
-const path = require('path');
-const fs = require('fs');
-const formidable = require('formidable');
-const { EOL } = require('os');
-
-const server = http.createServer((req, res) => {
-    if (req.url === '/'  && req.method === 'GET'){
-        res.writeHead(200, {
-            'content-type': 'text/html',
-        })
 
 
-        res.write(`
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Work with Forms</title>
-            </head>
-            <body>
-                <form action="/upload2" method="POST" enctype="multipart/form-data">
-                    <div>
-                        <label for="username">Username</label>
-                        <input type="text" name="username" id="username" />
-                    </div>
+// const http = require('http');
+// const querystring = require('querystring');
+// const path = require('path');
+// const fs = require('fs');
+// const formidable = require('formidable');
+// const { EOL } = require('os');
 
-                    <div>
-                        <label for="password">Password</label>
-                        <input type="password" name="password" id="password" />
-                    </div>
+// const server = http.createServer((req, res) => {
+//     if (req.url === '/'  && req.method === 'GET'){
+//         res.writeHead(200, {
+//             'content-type': 'text/html',
+//         })
 
-                    <div>
-                        <label for="file">Avatar</label>
-                        <input type="file" name="avatar" id="avatar" />
-                    </div>
 
-                    <div>
-                        <input type="submit" value="Register" />
-                    </div>
-                </form>
-            </body>
-            </html>
-            `)
-        res.end();
-        // Simple form
-    }else if(req.url === '/'  && req.method === 'POST'){
+//         res.write(`
+//             <!DOCTYPE html>
+//             <html lang="en">
+//             <head>
+//                 <meta charset="UTF-8">
+//                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//                 <title>Work with Forms</title>
+//             </head>
+//             <body>
+//                 <form action="/upload2" method="POST" enctype="multipart/form-data">
+//                     <div>
+//                         <label for="username">Username</label>
+//                         <input type="text" name="username" id="username" />
+//                     </div>
 
-        let body = '';
+//                     <div>
+//                         <label for="password">Password</label>
+//                         <input type="password" name="password" id="password" />
+//                     </div>
 
-        req.on('data', chunk => {
-            //console.log(chunk);
-            body += chunk;
+//                     <div>
+//                         <label for="file">Avatar</label>
+//                         <input type="file" name="avatar" id="avatar" />
+//                     </div>
+
+//                     <div>
+//                         <input type="submit" value="Register" />
+//                     </div>
+//                 </form>
+//             </body>
+//             </html>
+//             `)
+//         res.end();
+//         // Simple form
+//     }else if(req.url === '/'  && req.method === 'POST'){
+
+//         let body = '';
+
+//         req.on('data', chunk => {
+//             //console.log(chunk);
+//             body += chunk;
             
-        });
+//         });
 
-        req.on('close', () => {
+//         req.on('close', () => {
             
-            const data = querystring.parse(body);
+//             const data = querystring.parse(body);
 
-            console.log(body);
-            res.end(); 
-        });
+//             console.log(body);
+//             res.end(); 
+//         });
       
-    }else if(req.url === '/upload'  && req.method === 'POST'){
+//     }else if(req.url === '/upload'  && req.method === 'POST'){
 
-        const body = [];
-        req.on('data', chunk => {
-            body.push(chunk);
-        });
+//         const body = [];
+//         req.on('data', chunk => {
+//             body.push(chunk);
+//         });
 
-        req.on('close', () => {
-            const dataBuffer = Buffer.concat(body)
+//         req.on('close', () => {
+//             const dataBuffer = Buffer.concat(body)
 
-            const data = dataBuffer.toString('binary');
-            const boundary = req.headers['content-type'].split('boundary=').at(1);
-            const parts = data.split(`--${boundary}`);
+//             const data = dataBuffer.toString('binary');
+//             const boundary = req.headers['content-type'].split('boundary=').at(1);
+//             const parts = data.split(`--${boundary}`);
 
-            const [meta, imageData] = parts[3].split(EOL + EOL);
-            const fileName = meta.match(/filename="(.+)"/)[1];
-            const savePath = path.join(__dirname, 'upload', fileName);
-            fs.writeFile(savePath, imageData, 'binary', (err) =>{
-                if (err){
-                    return res.end();
-                }
-                console.log('Image Uploaded');
-                res.writeHead(302, {
-                    'location': '/'
-                });
-                res.end();
-            });
+//             const [meta, imageData] = parts[3].split(EOL + EOL);
+//             const fileName = meta.match(/filename="(.+)"/)[1];
+//             const savePath = path.join(__dirname, 'upload', fileName);
+//             fs.writeFile(savePath, imageData, 'binary', (err) =>{
+//                 if (err){
+//                     return res.end();
+//                 }
+//                 console.log('Image Uploaded');
+//                 res.writeHead(302, {
+//                     'location': '/'
+//                 });
+//                 res.end();
+//             });
             
-        });
-    }else if(req.url === '/upload2'  && req.method === 'POST'){
-        const form = new formidable.IncomingForm();
-        form.parse(req, (err, fields, files) => {
+//         });
+//     }else if(req.url === '/upload2'  && req.method === 'POST'){
+//         const form = new formidable.IncomingForm();
+//         form.parse(req, (err, fields, files) => {
 
-            const savePath = path.join(__dirname, 'uploads', files['avatar'].at(0).originalFilename);
+//             const savePath = path.join(__dirname, 'uploads', files['avatar'].at(0).originalFilename);
 
-            fs.copyFile(files['avatar'].at(0).filepath, savePath, (err) => {
-                console.log('Image uploaded');
-                res.end();
-            })
-        });
-    }
-});
+//             fs.copyFile(files['avatar'].at(0).filepath, savePath, (err) => {
+//                 console.log('Image uploaded');
+//                 res.end();
+//             })
+//         });
+//     }
+// });
 
-server.listen(2000);
-console.log('Server is listening on http://localhost:2000...');
+// server.listen(2000);
+// console.log('Server is listening on http://localhost:2000...');
